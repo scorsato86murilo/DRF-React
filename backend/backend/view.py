8ac594113a08dac5c6b3ea0d4d9fbb1ebe50f36b
Murilo Scorsato <scorsato86murilo@gmail.com>
2025-04-01 22:29:14 -0300

    User e Group

---
+++ backend/backend/view.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User, Group
+
+from .serializers import UserSerializer, GroupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
